505696047e4bc3f0c431f6e65b7130b19ab5d382
Kamil Kantorski <76277614+kantorski@users.noreply.github.com>
2021-02-01 19:56:29 +0100

    added solution to zadanie_7 in zjazd_3

--- zjazd_3/zadanie_7.js
+++ zjazd_3/zadanie_7.js
@@ -4,8 +4,12 @@ Treść zadania:
 Napisz funkcję `flatten` która dla przekazanej tablicy tablic zwróci tablicę składającą się z elementów tablic wewnętrznych.
 */
 
-
 /*
 Wersja zaawansowana:
 Dodaj obsługę dowolnego zagnieżdżenia tablic.
 */
+
+const flatArr = (arr) => {
+    let newArr = arr.flat(Infinity)
+    return newArr
+}
